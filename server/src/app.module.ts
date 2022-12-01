6053491c126a2498c8c43eb2365bf86028b0a1ec
Alyona Korenkovich <al.korenkovich@mail.ru>
2022-12-01 16:52:24 +0300

    #2: Add authentication logic (server).

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,15 +2,22 @@ import { Module } from '@nestjs/common';
 import { NoteModule } from './note/note.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
-// temporal
 import { DB_USERNAME, DB_PASSWORD } from './const/config';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
+    UsersModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     NoteModule,
     MongooseModule.forRoot(
       `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.tbht77d.mongodb.net/?retryWrites=true&w=majority`,
     ),
+    AuthModule,
   ],
 })
 export class AppModule {}
