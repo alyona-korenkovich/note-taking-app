19a3ea761cd7d4eb112c2689579aaf12fe87d62b
Alyona Korenkovich <al.korenkovich@mail.ru>
2022-12-02 11:22:22 +0300

    #6: Move sensitive data to .env file and replace the corresponding refs within the code

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { NoteModule } from './note/note.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
-import { DB_USERNAME, DB_PASSWORD } from './const/config';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
@@ -15,7 +14,7 @@ import { AuthModule } from './auth/auth.module';
     }),
     NoteModule,
     MongooseModule.forRoot(
-      `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.tbht77d.mongodb.net/?retryWrites=true&w=majority`,
+      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.tbht77d.mongodb.net/?retryWrites=true&w=majority`,
     ),
     AuthModule,
   ],
--- server/src/const/config.ts
+++ server/src/const/config.ts
@@ -1,5 +1,1 @@
 export const SALT = 10;
-
-export const DB_USERNAME = 'cri_decoeur';
-export const DB_PASSWORD = 'NJEJPZG4btrVrR';
-export const JWT_SECRET = 'jwt-dev';
